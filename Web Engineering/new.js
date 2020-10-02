5d08492e0ce6a513fea1ce4a669207cc4b085885
Aftab <44188234+aftalam@users.noreply.github.com>
2020-10-03 00:42:35 +0530

    Add files via upload

---
+++ Web Engineering/new.js
@@ -0,0 +1,37 @@
+var ans = "";
+var clear = false;
+var calc = "";
+$(function () {
+  $("button").click(function () {
+    var text = $(this).attr("value");
+    if (
+      parseInt(text, 10) == text ||
+      text === "." ||
+      text === "/" ||
+      text === "*" ||
+      text === "-" ||
+      text === "+" ||
+      text === "%"
+    ) {
+      if (clear === false) {
+        calc += text;
+        $(".textbox").val(calc);
+      } else {
+        calc = text;
+        $(".textbox").val(calc);
+        clear = false;
+      }
+    }
+    if (text === "AC") {
+      calc = "";
+      $(".textbox").val("");
+    } else if (text === "CE") {
+      calc = calc.slice(0, -1);
+      $(".textbox").val(calc);
+    } else if (text === "=") {
+      ans = eval(calc);
+      $(".textbox").val(ans);
+      clear = true;
+    }
+  });
+});
